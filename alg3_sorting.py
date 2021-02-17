af5985a560ecaef8424b51d318beff5222a99b54
Mark Foley <mark@markfoley.info>
2021-02-17 23:00:00 +0000

    Updated alg3_sorting.py and tk_demo0.py

--- alg3_sorting.py
+++ alg3_sorting.py
@@ -1,12 +1,26 @@
 # Bubble Sort
 # It is a comparison-based algorithm in which each pair of adjacent elements is compared and the elements are swapped
 # if they are not in order.
+#
+# Bubble Sort is one of the most straightforward sorting algorithms. Its name comes from the way the algorithm works:
+# With every new pass, the largest element in the list “bubbles up” toward its correct position.
+#
+# Bubble sort consists of making multiple passes through a list, comparing elements one by one, and swapping adjacent
+# items that are out of order.
 
 def bubble_sort(list):
     # Swap the elements to arrange in order
+
+    # Start looking at each item of the list one by one,
+    # comparing it with its adjacent value. With each
+    # iteration, the portion of the array that you look at
+    # shrinks because the remaining items have already been
+    # sorted.
     for iter_num in range(len(list) - 1, 0, -1):
         for idx in range(iter_num):
             if list[idx] > list[idx + 1]:
+                # If the item you're looking at is greater than its
+                # adjacent value, then swap them
                 temp = list[idx]
                 list[idx] = list[idx + 1]
                 list[idx + 1] = temp
@@ -20,11 +34,24 @@ print(f"{list}")
 
 
 # Merge Sort Merge sort first divides the array into equal halves and then combines them in a sorted manner.
+#
+# Merge sort is a very efficient sorting algorithm. It’s based on the divide-and-conquer approach, a powerful algorithmic
+# technique used to solve complex problems.
+#
+# To properly understand divide and conquer, you should first understand the concept of recursion. Recursion involves
+# breaking a problem down into smaller subproblems until they’re small enough to manage. In programming, recursion is
+# usually expressed by a function calling itself.
 def merge_sort(unsorted_list):
+    # If the input array contains fewer than two elements,
+    # then return it as the result of the function
     if len(unsorted_list) <= 1:
         return unsorted_list
     # Find the middle point and devide it
     middle = len(unsorted_list) // 2
+
+    # Sort the array by recursively splitting the input
+    # into two equal halves, sorting each half and merging them
+    # together into the final result
     left_list = unsorted_list[:middle]
     right_list = unsorted_list[middle:]
 
@@ -36,13 +63,23 @@ def merge_sort(unsorted_list):
 # Merge the sorted halves
 def merge(left_half, right_half):
     res = []
+
+    # Now go through both arrays until all the elements
+    # make it into the resultant array
     while len(left_half) != 0 and len(right_half) != 0:
+        # The elements need to be sorted to add them to the
+        # resultant array, so you need to decide whether to get
+        # the next element from the first or the second array
         if left_half[0] < right_half[0]:
             res.append(left_half[0])
             left_half.remove(left_half[0])
         else:
             res.append(right_half[0])
             right_half.remove(right_half[0])
+
+    # If you reach the end of either array, then you can
+    # add the remaining elements from the other array to
+    # the result and break the loop
     if len(left_half) == 0:
         res = res + right_half
     else:
@@ -56,20 +93,40 @@ print(f"Merge sort: {unsorted_list} -> {merge_sort(unsorted_list)}")
 
 
 # Insertion Sort
-# Insertion sort involves finding the right place for a given element in a sorted list.So in beginning we compare the
+# Insertion sort involves finding the right place for a given element in a sorted list. So in beginning we compare the
 # first two elements and sort them by comparing them. Then we pick the third element and find its proper position among
-# the previous two sorted elements.This way we gradually go on adding more elements to the already sorted list by putting
+# the previous two sorted elements. This way we gradually go on adding more elements to the already sorted list by putting
 # them in their proper position.
-def insertion_sort(InputList):
-    for i in range(1, len(InputList)):
+#
+# Like bubble sort, the insertion sort algorithm is straightforward to implement and understand. But unlike bubble sort,
+# it builds the sorted list one element at a time by comparing each item with the rest of the list and inserting it into
+# its correct position. This “insertion” procedure gives the algorithm its name.
+def insertion_sort(input_list):
+    # Loop from the second element of the array until
+    # the last element
+    for i in range(1, len(input_list)):
+        # Initialize the variable that will be used to
+        # find the correct position of the element
         j = i - 1
-        nxt_element = InputList[i]
+        # This is the element we want to position in its
+        # correct place
+        nxt_element = input_list[i]
         # Compare the current element with next one
 
-        while (InputList[j] > nxt_element) and (j >= 0):
-            InputList[j + 1] = InputList[j]
+        # Run through the list of items (the left
+        # portion of the array) and find the correct position
+        # of the element referenced by `key_item`. Do this only
+        # if `key_item` is smaller than its adjacent values.
+        while (input_list[j] > nxt_element) and (j >= 0):
+            # Shift the value one position to the left
+            # and reposition j to point to the next element
+            # (from right to left)
+            input_list[j + 1] = input_list[j]
             j = j - 1
-        InputList[j + 1] = nxt_element
+
+        # When you finish shifting the elements, you can position
+        # `nxt_element` in its correct location
+        input_list[j + 1] = nxt_element
 
 
 list = [19, 2, 31, 45, 30, 11, 121, 27]
@@ -86,21 +143,33 @@ print(f"{list}")
 # entire list is sorted.
 
 def shell_sort(input_list):
+    # Start with a big gap, then reduce the gap
     gap = len(input_list) // 2
+
+    # Do a gapped insertion sort for this gap size.
+    # The first gap elements a[0..gap-1] are already in gapped
+    # order keep adding one more element until the entire array
+    # is gap sorted
     while gap > 0:
 
         for i in range(gap, len(input_list)):
+            # add a[i] to the elements that have been gap sorted
+            # save a[i] in temp and make a hole at position i
             temp = input_list[i]
+
+            # shift earlier gap-sorted elements up until the correct
+            # location for a[i] is found
             j = i
             # Sort the sub list for this gap
 
             while j >= gap and input_list[j - gap] > temp:
                 input_list[j] = input_list[j - gap]
                 j = j - gap
+
+            # put temp (the original a[i]) in its correct location
             input_list[j] = temp
 
         # Reduce the gap for the next element
-
         gap = gap // 2
 
 
--- tk_demo0.py
+++ tk_demo0.py
@@ -10,12 +10,12 @@ def handle_B():
 def handle_C():
     print("Wrong! Try again!")
 
+
 # ------------ task 1: define the appearance of the screen ------------
 print("\n" * 100)  # clear the screen
 print("            VERY CHALLENGING GUESSING GAME\n========================================================\nPress the" \
-    "letter of your answer, then the ENTER key.\n\n    A.  Animal\n    B.  Vegetable\n    C.  Mineral\n\n    X.  Exit"\
-    "from self program\n\n========================================================\nWhat kind of thing is 'Trillium'?\n")
-
+      "letter of your answer, then the ENTER key.\n\n    A.  Animal\n    B.  Vegetable\n    C.  Mineral\n\n    X.  Exit" \
+      "from self program\n\n========================================================\nWhat kind of thing is 'Trillium'?\n")
 
 # ---- task 4: the event loop.  We loop forever, observing events. ---
 while True:
@@ -38,4 +38,4 @@ while True:
         print("\n" * 100)
         break
 
-        # Note that any other events are uninteresting, and are ignored
+    # Note that any other events are uninteresting, and are ignored
